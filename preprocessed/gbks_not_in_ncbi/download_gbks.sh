db3a881764b24ae798b7409ad8228ebc42de7933
Satria Ardhe Kautsar <satriaphd@gmail.com>
2019-04-15 19:23:52 +0200

    last commit before manual review with sjshaw

---
+++ preprocessed/gbks_not_in_ncbi/download_gbks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget --output-document BGC0000299.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0000299/BGC0000299.1.cluster001.gbk
+wget --output-document BGC0001129.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001129/BGC0001129.1.cluster001.gbk
+wget --output-document BGC0001315.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001315/BGC0001315.1.cluster001.gbk
+wget --output-document BGC0001316.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001316/BGC0001316.1.cluster001.gbk
+wget --output-document BGC0001317.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001317/BGC0001317.1.cluster001.gbk
+wget --output-document BGC0001318.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001318/BGC0001318.1.cluster001.gbk
+wget --output-document BGC0001319.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001319/BGC0001319.1.cluster001.gbk
+wget --output-document BGC0001319.2.gbk https://mibig.secondarymetabolites.org/repository/BGC0001319/BGC0001319.1.cluster002.gbk
+wget --output-document BGC0001320.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001320/BGC0001320.1.cluster001.gbk
+wget --output-document BGC0001320.2.gbk https://mibig.secondarymetabolites.org/repository/BGC0001320/BGC0001320.1.cluster002.gbk
+wget --output-document BGC0001325.1.gbk https://mibig.secondarymetabolites.org/repository/BGC0001325/BGC0001325.1.cluster001.gbk
